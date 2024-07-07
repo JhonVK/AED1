5f2d6be032e6733a893fa0c9fa58195446a6d21d
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-07-07 13:02:35 -0300

    update

--- Tarefa1AED.c
+++ Tarefa1AED.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+void *alocarPessoa(char *nome, int *idade, char *email){
+    void *pessoa=malloc(sizeof(nome)*50 + sizeof(idade)+ sizeof(email)*50+ sizeof(void*)*2);
+    memcpy(pessoa, nome, 50);
+    memcpy(pessoa + 50, idade, sizeof(int));
+    memcpy(pessoa + 50 + sizeof(int), email, 50);
+    }
+
+
+
 int main() {
-    
+
+
+
     return 0;
 }
