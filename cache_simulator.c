7cd4454fb836c9bb07fce75026780fe5ece99808
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-09-12 16:06:20 -0300

    update

--- cache_simulator.c
+++ cache_simulator.c
@@ -1,21 +1,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main( int argc, char *argv[ ] )
-{
+void leituraDados(int *argc, char ***argv){
+   
+    char string[50];
+    char *token;
+    
+    fgets(string, 50, stdin);
+
+    token = strtok(string, " ");
+
+    while(token!=NULL){
+        *argv = realloc(*argv, sizeof(char*)*((*argc) + 1));
+        (*argv)[*argc]=malloc(sizeof(char)*50);
+        strcpy((*argv)[*argc], token);
+        token=strtok(NULL, " ");
+        (*argc)++;
+    }
+}
+
+int main(int argc, char *argv[]){
+	argc=0;
+	leituraDados(&argc, &argv);
 	if (argc != 7){
 		printf("Numero de argumentos incorreto. Utilize:\n");
 		printf("./cache_simulator <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada\n");
 		exit(EXIT_FAILURE);
 	}
+
 	int nsets = atoi(argv[1]);
 	int bsize = atoi(argv[2]);
 	int assoc = atoi(argv[3]);
 	char *subst = argv[4];
 	int flagOut = atoi(argv[5]);
 	char *arquivoEntrada = argv[6];
-
 
 	printf("nsets = %d\n", nsets);
 	printf("bsize = %d\n", bsize);
@@ -24,8 +44,6 @@ int main( int argc, char *argv[ ] )
 	printf("flagOut = %d\n", flagOut);
 	printf("arquivo = %s\n", arquivoEntrada);
 
-
-    // Seu codigo vai aqui
 
 
 	return 0;
