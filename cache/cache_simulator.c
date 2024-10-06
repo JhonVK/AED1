50aa803ee9439b8cb1d800a0745fa76ebbe8b47b
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-10-06 15:14:15 -0300

    update

--- cache/cache_simulator.c
+++ cache/cache_simulator.c
@@ -10,7 +10,6 @@ void leituraDados(int *argc, char ***argv){ //argv aqui é o endereço do pontei
     char string[50];
     char *token;
     
-
     fgets(string, 50, stdin);
 
 	int tamanho = strlen(string); // removendo o \n que o fgets concatena no final da leitura
@@ -32,11 +31,11 @@ void leituraDados(int *argc, char ***argv){ //argv aqui é o endereço do pontei
 void criarCache(int nsets, int bsize, int assoc, char *subst, int flagOut, char *arquivoEntrada) {
     FILE *arquivo;
     unsigned char buffer[4];
-    unsigned int endereco; // uns int tem 4 bytes= 32 bits
+    unsigned int endereco=0, tag=0, indice=0, hitTemp=0, hit=0; // uns int tem 4 bytes= 32 bits cache_simulator 256 4 1 R 1 bin_100.bin
 
     arquivo = fopen(arquivoEntrada, "rb");
 
-    if (arquivo == NULL) {
+    if(arquivo == NULL){
         printf("Erro ao ler arquivo\n");
         exit(1);
     }
@@ -52,9 +51,29 @@ void criarCache(int nsets, int bsize, int assoc, char *subst, int flagOut, char 
     printf("indice: %d\n", n_bits_indice);
     printf("tag: %d\n", n_bits_tag);
 
-    while (fread(buffer, sizeof(unsigned char), 4, arquivo) == 4) { // uns char tem 8 bits, 8*4= 32 bits
-    endereco= buffer[0]<<24 | buffer[1]<<16 | buffer[2]<<8 | buffer[3];// por algum motivo se lermos direto, (32 bits de uma vez) o endereço fica invertido, por isso li byte por byte e dps inverti eles
+    while(fread(buffer, sizeof(unsigned char), 4, arquivo) == 4) { // uns char tem 8 bits, 8*4= 32 bits
+    	endereco= buffer[0]<<24 | buffer[1]<<16 | buffer[2]<<8 | buffer[3];// por algum motivo se lermos direto, (32 bits de uma vez) o endereço fica em little endian.Por isso li byte por byte e desloquei eles para big endian
+		tag = endereco>>(n_bits_offset + n_bits_indice);
+		indice = (endereco >> n_bits_offset) & ((unsigned int)pow(2, n_bits_indice)-1); 
+		printf("Tag: %d, Indice: %d\n", tag, indice);
 
+
+		for(int i=0; i<assoc; i++){
+			if(cache_val[indice][assoc]==1 && cache_tag[indice][assoc]==tag){
+				hitTemp=1;
+			}
+		}
+		if(hitTemp=0){//(MISS)
+			if(*subst=='R'){
+				int randBloco=rand()%assoc;
+				cache_val[indice][randBloco]=1;
+				cache_tag[indice][randBloco]=tag;
+			}
+		}else{
+			hit+=hitTemp;
+			hitTemp=0;
+		}
+		printf("Total de hits: %d", hit);
     }
 
     fclose(arquivo);
